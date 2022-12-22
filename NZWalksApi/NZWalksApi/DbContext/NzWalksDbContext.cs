56d161ca2f69863d99656a95ac4a017f5501293f
Fausto E. Rodriguez <faustoerv@gmail.com>
2022-12-22 08:58:33 -0400

    End section 2

---
+++ NZWalksApi/NZWalksApi/DbContext/NzWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksApi.Models;
+
+namespace NZWalksApi.DbContext
+{
+    public class NzWalksDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public NzWalksDbContext(DbContextOptions<NzWalksDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
